6b042c2beb9fe8213299f1c4a32edf2ec4ffcc39
Amilenkof <f.o.g@list.ru>
2023-05-29 20:21:05 +0400

    init commit

---
+++ src/test/java/com/example/hw211spring/Hw211SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw211spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw211SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
