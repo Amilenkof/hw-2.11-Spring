e670b2bb41228181df19cb171f4483f103f57e05
Amilenkof <f.o.g@list.ru>
2023-05-29 21:36:55 +0400

    homework is done red

--- src/main/java/com/example/hw211spring/BasketService.java
+++ src/main/java/com/example/hw211spring/BasketService.java
@@ -9,26 +9,27 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @Service
-//@Scope("prototype")
+
 @SessionScope
 public class BasketService {
-    private ArrayList <Item> basket;
+    private ArrayList<Item> basket;
 
     public BasketService(ArrayList<Item> basket) {
         this.basket = basket;
     }
 
-    String add (int ... itemID) {
-        int size=basket.size();
-        ArrayList<Item> addToBasket = (ArrayList<Item>)( Arrays.stream(itemID)
+    String add(int... itemID) {
+        int size = basket.size();
+        ArrayList<Item> addToBasket = (ArrayList<Item>) (Arrays.stream(itemID)
                 .mapToObj(e -> new Item(e))
                 .collect(Collectors.toList()));
-        boolean checkAdd=basket.addAll(addToBasket);
+        boolean checkAdd = basket.addAll(addToBasket);
         return checkAdd ? "Items added" : "Wrong add";
 
 
     }
-     ArrayList<Item> getBasket () {
+
+    ArrayList<Item> getBasket() {
         return basket;
     }
 
